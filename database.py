973d8a6fd03baa5fb177ec8b4bcd399bac9604a5
Aminov Emil <nailami60@mail.ru>
2019-04-19 15:37:57 +0300

    added first functional

---
+++ database.py
@@ -0,0 +1,18 @@
+import psycopg2 as psycopg2
+
+
+def insert_words_list(kadastr_num, address, link_of_kadastr_num, floor, json):
+    sql = "INSERT INTO property_object(kadastr_num, adress, link_of_kadastr_num) VALUES(%s,%s,%s)"
+    conn = None
+    try:
+        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres",
+                                password="megavova")
+        cur = conn.cursor()
+        cur.execute(sql, (kadastr_num, address, link_of_kadastr_num))
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
---
+++ parser.py
@@ -0,0 +1,33 @@
+import json
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+from database import insert_words_list
+
+
+def give_post_to_service():
+    url = "https://extra.egrp365.ru/api/extra/index.php"
+    data = {
+        "macroRegionId": "192000000000",
+        "regionId": "192401000000",
+        "settlementId": "192401380000",
+        "streetType": "str1",
+        "street": "Глазунова",
+        "method": "searchByAddress"
+    }
+    headers = {'user-agent': 'Mozilla/5.0'}
+    return requests.post(url, data=data, headers=headers)
+
+
+if __name__ == '__main__':
+    resp = give_post_to_service().text
+    elements = json.loads(resp)['data']
+    for el in elements:
+        print(el['cn'])
+        print(el['address'])
+        print('\n')
+        link = 'https://egrp365.ru/reestr?egrp='+el['cn']
+        insert_words_list(kadastr_num=el['cn'],address=el['address'],link_of_kadastr_num=link,floor=None,json=None)
+    # json.dumps(json_text, sort_keys=True, indent=4)
