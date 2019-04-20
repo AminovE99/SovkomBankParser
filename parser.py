0ec6417ec709383936cacb4da9bea9cedc5594c7
Aminov Emil <nailami60@mail.ru>
2019-04-21 00:26:54 +0300

    error with response

--- parser.py
+++ parser.py
@@ -4,23 +4,17 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-from dadata import get_useful_info
+from dadata import get_useful_info_from_dadata
 from database import insert_words_list
 
-
 '''
-streetType: str1
-street: Репищева
-apartment: 258
-method: searchByAddress
+
+give_post_to_extra_service - эта функция находит по одной строке информацию в url /extra
+give_get_to_service - Эта функция находит на главной странице всю информацию по одной строке
 '''
 
 
-
-
-
-def give_post_to_service(address,info):
-    i = 1
+def give_post_to_extra_service(address, info):
     macroRegionId = "1" + str(int(info['okato']) // 1000000000) + "000000000"
     regionId = "1" + str(int(info['okato']) // 1000000) + "000000"
     settlementId = "1" + str(int(info['okato']) // 1000) + "000"
@@ -40,6 +34,23 @@ def give_post_to_service(address,info):
     return requests.post(url, data=data, headers=headers)
 
 
+def give_get_to_service(info):
+    url = "https://egrp365.ru/list4.php"
+    data = {
+        "street": info['street'],
+        "house": info['house_num'],
+        "mregion": info['region'],
+        "building": info['building_num'],
+        "city": info['city'],
+        "apartment": info['flat_num'],
+        "area": None,
+        "link": "page",
+        "fias_id": info['fias_id']
+    }
+    headers = {'user-agent': 'Mozilla/5.0'}
+    return requests.get(url, data=data)
+
+
 def get_space_and_floor_and_metres(link):
     soup = BeautifulSoup(''.join(requests.get(link).text))
     info = soup.find('div', {"id": "information_about_object"}).contents
@@ -51,15 +62,19 @@ def get_space_and_floor_and_metres(link):
     except IndexError:
         print('этажа нет')
     metres = info.split('Площадь —')[1].split("<br")[0]
-    info_dict = {'place': place, 'floor': floor, 'metres': metres,}
+    info_dict = {'place': place, 'floor': floor, 'metres': metres}
+    print("info_dict" + str(info_dict))
     return info_dict
 
 
 if __name__ == '__main__':
     address = input("Напишите адрес, где расположен дом: ")
-    info = get_useful_info(address)
-    resp = give_post_to_service(address,info).text
+    info = get_useful_info_from_dadata(address)
+    resp = give_post_to_extra_service(address, info).text
+    resp2 = give_get_to_service(info).text
     elements = json.loads(resp)['data']
+    if not elements:
+        print("Информация не найдена")
     for el in elements:
         link = 'https://egrp365.ru/reestr?egrp=' + el['cn']
         info_dict = get_space_and_floor_and_metres(link)
